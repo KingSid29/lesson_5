c8e0ef8c88bb09eb6872f7199c97af10e7412c60
KingSid29 <siddhantmittal103@gmail.com>
2026-09-14 17:13:48 +0100

    activity 3

---
+++ activity_3.py
@@ -0,0 +1,8 @@
+num = int(input("Enter your number"))
+if num < 50:
+    print("number is less then 50")
+    print("I am in if block")
+else:
+    print("number is greater then 50")
+    print("I'm in else block")
+print("I'm nethier in if nor in else block")
